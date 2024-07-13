e5ed4825447252efb3420e44a5ba7f1a93f3677c
Developer Siddarth <142804820+Siddarth-abcs@users.noreply.github.com>
2024-07-13 17:27:19 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="mt-36 sm:bg-black">
+    <div className="mt-36">
       <section className="body-font relative text-gray-400">
         <div className="container mx-auto px-5 py-24">
           <div className="mb-12 flex w-full flex-col text-center">
